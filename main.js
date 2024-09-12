69da53a2d38a44d891806c47649b5736424ffdee
Tron <109302537+MrDonaldGlov@users.noreply.github.com>
2024-09-12 13:25:30 +0000

    owen josh brandon

--- main.js
+++ main.js
@@ -11,15 +11,25 @@ let app = document.querySelector("#app");
 // Create a new "Text Interface"
 
 let ti = new TextInterface(app, " Family Guy Quiz");
-ti.output("I'll try to guess which family guy character you are");
+ti.output("I'll try to guess which family guy character you are (hit return to begin)");
+await ti.readText();
+ti.clear();
+ti.output("Are you male!")
+if (await ti.readYesOrNo()) {
+  ti.output('Freakin sweet')
+} else {
+  ti.output("Shut up meg")
+}
 //if you type ryan, tedddy or aditiya automatically do "Greased up deaf guy and end game"
 //are you male or female 
-//are you an animal
-//are part of a large family?
+//are currently part of the Griffin family?
+//do you have a son
 //do you get picked on a lot?
 //are you an adult 
 // is rich?
-//
+//are you a child
+//do you have a
+//are you an animal
 //If you choose yes for male or female try to figure out a way to store the data while asking the other questions
 //Avalable answers Peter Brian Stewie Joe Quagmire Cleaveland Chris beurtrum , Ladies Lois Ellie(dog) Meg Bonnie Trica Donna Babs 
 // find a way to store the saved yes or no answer to question Like if char is male or female store it as 1 for yes or 0 for no in he end the binary should be like 10010 whatever = brian not sure how to do that yet.
