63a0cd0bf7a51c4e92b7c0aadabddc4b2e471c04
Tron <109302537+MrDonaldGlov@users.noreply.github.com>
2024-09-12 12:39:18 +0000

    Latest comments

--- main.js
+++ main.js
@@ -10,7 +10,9 @@ ti.output("I'll try to guess which family guy character you are");
 //are you an animal
 //are part of a large family?
 //do you get picked on a lot?
+//are you an adult 
+// is rich?
 //
 //If you choose yes for male or female try to figure out a way to store the data while asking the other questions
-//Avalable answers Peter Brian Stewie Joe Quagmire Cleaveland Chris Adam west, Ladies Lois Ellie(dog) Meg Bonnie Trica Donna Babs 
+//Avalable answers Peter Brian Stewie Joe Quagmire Cleaveland Chris beurtrum , Ladies Lois Ellie(dog) Meg Bonnie Trica Donna Babs 
 // find a way to store the saved yes or no answer to question Like if char is male or female store it as 1 for yes or 0 for no in he end the binary should be like 10010 whatever = brian not sure how to do that yet.
