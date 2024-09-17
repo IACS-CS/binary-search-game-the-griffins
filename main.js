4fcdf88b4c6028be5a42d1458cca9e67f6b249e1
Tron <109302537+MrDonaldGlov@users.noreply.github.com>
2024-09-17 14:51:06 +0000

    Final Draft?

--- main.js
+++ main.js
@@ -23,13 +23,18 @@ if (await ti.readYesOrNo()) {
 //if you type ryan, tedddy or aditiya automatically do "Greased up deaf guy and end game"
 //are you male or female 
 //are currently part of the Griffin family?
+//do you live with the browns?
 //do you have a son
 //do you get picked on a lot?
 //are you an adult 
-// is rich?
+//is rich?
 //are you a child
 //do you have a
 //are you an animal
+// do you like icescream 
+//were you famous?
+//Do you have a husband or wife?
+//are you smart?
 //If you choose yes for male or female try to figure out a way to store the data while asking the other questions
-//Avalable answers Peter Brian Stewie Joe Quagmire Cleaveland Chris beurtrum , Ladies Lois Ellie(dog) Meg Bonnie Trica Donna Babs 
+//Avalable answers Peter Brian Stewie Joe Quagmire Cleaveland Chris cleaveland jr , Ladies Lois Ellie(dog) Meg Bonnie Trica Donna Babs 
 // find a way to store the saved yes or no answer to question Like if char is male or female store it as 1 for yes or 0 for no in he end the binary should be like 10010 whatever = brian not sure how to do that yet.
